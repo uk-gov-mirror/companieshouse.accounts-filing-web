c304f016fff06818c58602e75854776ec2957c78
jbox-CH <jbox@companieshouse.gov.uk>
2026-08-24 10:29:49 +0100

    removed FIXME

--- src/routers/handlers/errors/csrf.error.ts
+++ src/routers/handlers/errors/csrf.error.ts
@@ -8,7 +8,7 @@ export const csrfErrorHandler = (err: CsrfError | Error, _: Request,
                                  res: Response, next: NextFunction) => {
 
     // handle non-CSRF Errors immediately
-    if (!(err instanceof CsrfError)) { // FIXME: CsrfError can be undefined at runtime crashing
+    if (!(err instanceof CsrfError)) { // CsrfError can be undefined at runtime crashing
         next(err);
         return;
     }
